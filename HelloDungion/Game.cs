6cb2d1ca97994d78ff04ba8ce3172acb9df682d0
Will <montero_w@live.com>
2021-08-18 13:07:13 -0500

    Set more values and description for user
    Added Values to skill chooses for user for 'Experience' and 'Levels'.

--- HelloDungion/Game.cs
+++ HelloDungion/Game.cs
@@ -6,14 +6,13 @@ namespace HelloDungion
 {
     class Game
     {
-        int dummyCount;
         public void Run()
         {
-            
+
             // values to for dementration
             //int health = 100; // value for health 
             //int healthRegen = 50; //values for health regen
-            
+
             // Increases health by regeon ammount
             //health += 200; 
             //health += healthRegen;
@@ -28,76 +27,94 @@ namespace HelloDungion
             //name = Console.ReadLine();
             // Takes Input from the user
 
-           //Console.WriteLine("So Your name is " + name);
+            //Console.WriteLine("So Your name is " + name);
 
             // Users name place holder
             string userName = "";
+
             // Job or task there will take on
-            string userJob = "Novsie";
+            string userJob = "NOVISE";
+
+            // Checks for Player Response
+            string playerResponse = "";
+
             // Current level they reside in 
             int userLevel = 0;
+
+            // Health value
+            int userHealth = 0;
+
+            // Hold Charaters Damage output
+            int userDamage = 0;
+
             // Check for Correct Spelling on the job 
             float earningExp = 0;
 
+            //Definds gameOver variable for use Later 
+            bool gameOver = true;
 
 
 
             // Introduction
-            Console.Write("So You Found Your Way Here Novise and Since Your Here I Can Only Assume YOu Came Here to Gain Some Lelevs in Your Super Hero States");
+            Console.Write("So You Found Your Way Here Novise and Since Your Here I Can Only Assume You Came Here to Gain Some Leveles For Your Super Hero States \n");
             // Spaces Out The Lines So It Looks Cleaner 
             Console.ReadLine();
+
+
             // Takes User Input and Stores It in userName
-            Console.WriteLine("Lets Start With Your Name Seems Easy Enough ");
+            Console.WriteLine("Lets Start With Your Name, Seems Easy Enough \n");
+            Console.WriteLine("> ");
             userName = Console.ReadLine();
 
             // Takes User Input and Stores It in userJob
-            Console.WriteLine("So Tell me What You Currently do This is Important for This Will Dectate how much Exp You'll Gain");
-            Console.ReadLine();
-            Console.WriteLine("Are You a 'Flying' Type");
-            Console.ReadLine();
-            Console.WriteLine("Are You a 'Speedster' Type ");
-            Console.ReadLine();
-            Console.WriteLine("Are You a 'Acrobatic' Type ");
+            Console.WriteLine("So Tell me What Kind of Skill do You Posses, This is Important for This Will Dectate how much Exp You'll Gain");
             Console.ReadLine();
 
-            if (userJob == "Flayer")
+            // Asks from the veriations of occupation
+            Console.WriteLine("Are You a 'Flying Enter (1)' Type");
+            Console.WriteLine();
+            Console.WriteLine("Are You a 'Speedster Enter (2)' Type ");
+            Console.WriteLine();
+            Console.WriteLine("Are You a 'Acrobatic Enter (3)' Type ");
+            Console.WriteLine("> ");
+            playerResponse = Console.ReadLine();
+
+            //Users Resposnse in order to Dictate what will happen to the User 
+            if (playerResponse == "1" || playerResponse == "Flyer") // Input Response to (1) or Flyer sets Characters capabilities 
             {
-                earningExp += 12;
+                userJob = "Flyer";
+                Console.WriteLine("So I See You're a Flyer, not the strogest ability but has the better survivability of the other two ");
+                userLevel = 20;
+                earningExp += .05f;   
 
-                if (userLevel <= 100)
-                {
-
-                }
             }
-            if (userJob == "Speedster")
-            {
-
-
-                if (userLevel <= 100)
-                {
-
-                }
-            }
-            if (userJob == "Acrobotic")
+            else if (playerResponse == "2" || playerResponse == "Speedster")
             {
 
+                userJob = "Speedster";
+                Console.WriteLine("So I See You're a Speedster, The Most Balanced of All the Other Classes ");
+                userLevel = 10;
+                earningExp += .1f;
 
-                if (userLevel <= 100)
-                {
-
-                }
             }
+            else if (playerResponse == "3" || playerResponse == "Acrobatic")
+            {
+                userJob = "Acrobatic";
+                Console.WriteLine("So I See a Acrobatic, The Strongest out off the Class but less likely to survive");
+                userLevel = 5; 
+                earningExp += 2;
+            }
+            else
+                Console.WriteLine(" Sorry but this is not the place for you to level up, Try somewhere else or TRY AGAIN " + userJob);
+
+            Console.ReadKey();
+            Console.Clear();
 
 
 
 
-            Console.WriteLine(" With Quick Intro Ductions Aside " + userName);
 
-            Console.WriteLine("");
-
-            Console.Read();
-
-        //Test Commit 
+            //Test Commit 
 
 
 
